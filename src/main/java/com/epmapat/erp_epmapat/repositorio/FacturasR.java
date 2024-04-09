e224557c6320be49f269be01e3563e309c2d2da7
ortizal19009 <ort19009@byui.edu>
2024-04-08 23:40:00 -0500

    update

--- src/main/java/com/epmapat/erp_epmapat/repositorio/FacturasR.java
+++ src/main/java/com/epmapat/erp_epmapat/repositorio/FacturasR.java
@@ -95,7 +95,7 @@ public interface FacturasR extends JpaRepository<Facturas, Long> {
 	@Query(value = "SELECT fc.descripcion AS formaCobro, SUM(rf.cantidad * rf.valorunitario) AS total FROM Facturas f "
 			+ "JOIN Rubroxfac rf ON rf.idfactura_facturas = f.idfactura "
 			+ "JOIN Formacobro fc ON fc.idformacobro = f.formapago "
-			+ "WHERE f.fechacobro = :fecha AND (f.estado=1 OR f.estado=2) AND f.fechaeliminacion IS NULL AND (f.fechaanulacion <= :fecha or f.fechaanulacion IS NULL)"
+			+ "WHERE f.fechacobro = :fecha AND (f.estado=1 OR f.estado=2) AND f.fechaeliminacion IS NULL AND (f.fechaanulacion <= ?1 or f.fechaanulacion IS NULL)"
 			+
 			" GROUP BY fc.descripcion ORDER BY fc.descripcion")
 	List<Object[]> totalFechaFormacobro(@Param("fecha") LocalDate fecha);
