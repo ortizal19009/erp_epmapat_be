9cf2af045b4034ffc0643bddf186d4eb061f077b
ortizal19009 <ort19009@byui.edu>
2024-05-15 00:15:30 -0500

    update

--- src/main/java/com/epmapat/erp_epmapat/modelo/EmisionIndividual.java
+++ src/main/java/com/epmapat/erp_epmapat/modelo/EmisionIndividual.java
@@ -1,9 +1,12 @@
 package com.epmapat.erp_epmapat.modelo;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -12,9 +15,19 @@ public class EmisionIndividual {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idemisionindividual;
-    private Long idemision;
-    private Long idlecturanueva;
-    private Long idlecturaanterior;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idemision")
+    private Emisiones idemision;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idlecturanueva")
+    private Lecturas idlecturanueva;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idlecturaanterior")
+    private Lecturas idlecturaanterior;
+
+    public EmisionIndividual() {
+        super();
+    }
 
     public Long getIdemisionindividual() {
         return idemisionindividual;
@@ -24,39 +37,36 @@ public class EmisionIndividual {
         this.idemisionindividual = idemisionindividual;
     }
 
-    public Long getIdemision() {
+    public Emisiones getIdemision() {
         return idemision;
     }
 
-    public void setIdemision(Long idemision) {
+    public void setIdemision(Emisiones idemision) {
         this.idemision = idemision;
     }
 
-    public Long getIdlecturanueva() {
+    public Lecturas getIdlecturanueva() {
         return idlecturanueva;
     }
 
-    public void setIdlecturanueva(Long idlecturanueva) {
+    public void setIdlecturanueva(Lecturas idlecturanueva) {
         this.idlecturanueva = idlecturanueva;
     }
 
-    public Long getIdlecturaanterior() {
+    public Lecturas getIdlecturaanterior() {
         return idlecturaanterior;
     }
 
-    public void setIdlecturaanterior(Long idlecturaanterior) {
+    public void setIdlecturaanterior(Lecturas idlecturaanterior) {
         this.idlecturaanterior = idlecturaanterior;
     }
 
-    public EmisionIndividual() {
-        super();
-    }
-
-    public EmisionIndividual(Long idemisionindividual, Long idemision, Long idlecturanueva, Long idlecturaanterior) {
+    public EmisionIndividual(Long idemisionindividual, Emisiones idemision, Lecturas idlecturanueva,
+            Lecturas idlecturaanterior) {
         this.idemisionindividual = idemisionindividual;
         this.idemision = idemision;
         this.idlecturanueva = idlecturanueva;
         this.idlecturaanterior = idlecturaanterior;
     }
-    
+
 }
