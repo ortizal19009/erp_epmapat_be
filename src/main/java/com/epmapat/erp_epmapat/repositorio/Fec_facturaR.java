81d68207151c82a40bef0f54f7575ef7cb10dac2
ortizln <ortizln9@gmail.com>
2024-05-23 16:30:02 -0500

    update

--- src/main/java/com/epmapat/erp_epmapat/repositorio/Fec_facturaR.java
+++ src/main/java/com/epmapat/erp_epmapat/repositorio/Fec_facturaR.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import com.epmapat.erp_epmapat.modelo.Fec_factura;
 
 public interface Fec_facturaR extends JpaRepository<Fec_factura, Long> {
-    @Query(value = "SELECT * FROM fec_factura where estado = ?1 order by idfactura asc limit = ?2 ", nativeQuery = true)
+    @Query(value = "SELECT * FROM fec_factura where estado like ?1 order by idfactura asc limit ?2 ", nativeQuery = true)
     public List<Fec_factura> findByEstado(String estado, Long limit);
 
     @Query(value = "SELECT * FROM fec_factura where referencia = ?1 order by idfactura asc ", nativeQuery = true)
