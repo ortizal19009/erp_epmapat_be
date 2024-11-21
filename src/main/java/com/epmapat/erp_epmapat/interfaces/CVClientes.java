99aa3db8ecade1c824bc062e81c760dfe5363748
ortizln <ortizln9@gmail.com>
2024-11-21 16:29:55 -0500

    update

--- src/main/java/com/epmapat/erp_epmapat/interfaces/CVClientes.java
+++ src/main/java/com/epmapat/erp_epmapat/interfaces/CVClientes.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 public interface CVClientes {
     String getPlanilla();
     BigDecimal getValor();
+    String getNombre();
     String getCedula();
     String getDireccion();
     String getEmail();
