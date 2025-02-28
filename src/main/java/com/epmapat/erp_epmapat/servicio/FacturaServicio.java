1deced476da8c3880931ec8f80be52e108b24a3b
ortizln <ortizln9@gmail.com>
2025-02-28 16:19:03 -0500

    update

--- src/main/java/com/epmapat/erp_epmapat/servicio/FacturaServicio.java
+++ src/main/java/com/epmapat/erp_epmapat/servicio/FacturaServicio.java
@@ -202,7 +202,6 @@ public class FacturaServicio {
 
 	/* transferencias cobradas */
 	public List<R_transferencias> transferenciasCobradas(Date d_fecha, Date h_fecha) {
-		System.out.println("Por aqui");
 		return this.dao.transferenciasCobradas(d_fecha, h_fecha);
 	}
 
