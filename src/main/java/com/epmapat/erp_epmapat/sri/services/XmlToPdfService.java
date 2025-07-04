2ccb0f1f78c78136153b86ecbcce92b3587b1a39
ortizln <ortizln9@gmail.com>
2025-07-04 16:31:29 -0500

    update datos

--- src/main/java/com/epmapat/erp_epmapat/sri/services/XmlToPdfService.java
+++ src/main/java/com/epmapat/erp_epmapat/sri/services/XmlToPdfService.java
@@ -21,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
