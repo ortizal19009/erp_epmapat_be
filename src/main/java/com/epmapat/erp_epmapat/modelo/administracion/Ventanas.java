1165cafffa474d6b3735fb02618ea0164265269f
ortizal <alexis.ortiz81@outlook.com>
2025-10-15 11:14:54 -0500

    update

--- src/main/java/com/epmapat/erp_epmapat/modelo/administracion/Ventanas.java
+++ src/main/java/com/epmapat/erp_epmapat/modelo/administracion/Ventanas.java
@@ -6,9 +6,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "ventanas")
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ventanas")
 public class Ventanas {
 
    @Id
@@ -19,36 +27,6 @@ public class Ventanas {
    private String color1;
    private String color2;
    private Long idusuario;
-   
-   public Long getIdventana() {
-      return idventana;
-   }
-   public void setIdventana(Long idventana) {
-      this.idventana = idventana;
-   }
-   public String getNombre() {
-      return nombre;
-   }
-   public void setNombre(String nombre) {
-      this.nombre = nombre;
-   }
-   public String getColor1() {
-      return color1;
-   }
-   public void setColor1(String color1) {
-      this.color1 = color1;
-   }
-   public String getColor2() {
-      return color2;
-   }
-   public void setColor2(String color2) {
-      this.color2 = color2;
-   }
-   public Long getIdusuario() {
-      return idusuario;
-   }
-   public void setIdusuario(Long idusuario) {
-      this.idusuario = idusuario;
-   }
+   private Long permissions;
 
 }
