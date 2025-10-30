20138ef7bfe955202e70ac5c77f0fc5e10d3e692
ortizal19009 <ort19009@byui.edu>
2025-10-30 07:41:27 -0500

    update facturas y ventanas

--- src/main/java/com/epmapat/erp_epmapat/modelo/Facturas.java
+++ src/main/java/com/epmapat/erp_epmapat/modelo/Facturas.java
@@ -2,10 +2,9 @@ package com.epmapat.erp_epmapat.modelo;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.TimeZone;
-
 import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -34,27 +33,15 @@ public class Facturas implements Serializable {
 	private BigDecimal totaltarifa;
 	private Integer pagado;
 	private Long usuariocobro;
-	// @Temporal(TemporalType.DATE)
-	// @DateTimeFormat(iso = ISO.DATE_TIME)
-	// @Column(name = "fechacobro")
 	private LocalDate fechacobro;
 	private Long estado;
 	private Long usuarioanulacion;
-	// @Temporal(TemporalType.DATE)
-	// @DateTimeFormat(iso = ISO.DATE)
-	// @Column(name = "fechaanulacion")
 	private LocalDate fechaanulacion;
 	private String razonanulacion;
 	private Long usuarioeliminacion;
-	// @Temporal(TemporalType.DATE)
-	// @DateTimeFormat(iso= ISO.DATE)
-	// @Column ( name = "fechaeliminacion")
 	private LocalDate fechaeliminacion;
 	private String razoneliminacion;
 	private Long conveniopago;
-	// @Temporal(TemporalType.DATE)
-	// @DateTimeFormat(iso= ISO.DATE)
-	// @Column(name = "fechaconvenio")
 	private LocalDate fechaconvenio;
 	private Long estadoconvenio;
 	private Long formapago;
@@ -62,20 +49,10 @@ public class Facturas implements Serializable {
 	@JsonFormat(pattern = "H:m:s")
 	private LocalTime horacobro;
 	private Long usuariotransferencia;
-	// @Temporal(TemporalType.DATE)
-	// @DateTimeFormat(iso = ISO.DATE)
-	// @Column(name = "fechatransferencia")
 	private LocalDate fechatransferencia;
 	private Long usucrea;
-
-	// @Temporal(TemporalType.DATE)
-	// @DateTimeFormat(iso=ISO.DATE)
-	// @Column(name = "feccrea")
 	private LocalDate feccrea;
 	private Long usumodi;
-	// @Temporal(TemporalType.DATE)
-	// @DateTimeFormat(iso = ISO.DATE)
-	// @Column(name = "fecmodi")
 	private LocalDate fecmodi;
 	private BigDecimal valorbase;
 	private Long idabonado;
@@ -84,5 +61,6 @@ public class Facturas implements Serializable {
 	private Boolean swcondonar; 
 	private BigDecimal valornotacredito;
 	private String secuencialfacilito;
+	private Timestamp fechacompensacion;
 
 }
